6047255bbfcf448785a74e3f0f90786851a32ed2
David Oliveira <doliveira.pz@gmail.com>
2023-10-12 14:56:21 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# node-message-router
-Node.js Multi-Backend Message Router
+# node-message-router - A Node.js powered multi-backend message router
+
+## Installing it
+	$ npm i -g message-router
+
+## Running it
+	$ mrouter --config=/path/for/config/file.json
+
+## Configuration
+
+### Minimum configuration
+
+{
+  "server": {
+    "proto": "tcp|udp"
+  },
+  "backends": {
+    "backend-one": {
+      "proto": "tcp|udp",
+      "host": "some.host.here",
+      "port": 1234
+    }
+  },
+  "backendSelector": [
+    {
+      ...rules here...
+      "rxMatches": "^{.*}$",
+      "parser": "json",
+      "bodyMatches": { "type": "meh" },
+      "target": "backend-one",
+    }
+  ]
+}
+
+
+### Supported selector rules
+
+- strContains: Accepts a string; Check if the message content contains the provided string;
+
+- rxMatches: Accepts a regular expression such as `"^regular expression$"`; Checks if the message content matches the provided RegExp;
+
+- parser: Accepts only "json" for now; Parses the message as JSON to then allow other rules to run, such as `bodyMatches`;
+
+- bodyMatches: Accepts an object; Checks whether the parsed object contains (at least) the provided object structure;
---
+++ bin/mrouter.js
@@ -0,0 +1,80 @@
+#!/usr/bin/env node
+
+const { UDPServer } = require('../lib/server/udp');
+const { TCPServer } = require('../lib/server/tcp');
+const { Backend } = require('../lib/backend');
+const config = require('../lib/config');
+const { isObject, objectMatches } = require('../lib/util');
+
+
+const BACKENDS = { };
+
+// Parse and prepare the config
+if (!config.backends || config.backends.length === 0) {
+  throw new Error('No backends have been configured. Stopping here!');
+}
+Object.keys(config.backends || []).forEach((id) => {
+  BACKENDS[id] = new Backend(id, config.backends[id]);
+});
+(config.backendSelector || []).forEach((rule) => {
+  if (rule.rxMatches) rule.rxMatches = new RegExp(rule.rxMatches);
+});
+
+
+function findBackend(msg) {
+  const parsedAs = { };
+
+  for (let rule of config.backendSelector) {
+    let parsedMsg;
+    if (!rule.target) continue;
+
+    // Bare matching options
+    if (rule.rxMatches && !msg.match(rule.rxMatches)) continue;
+    if (rule.strContains && msg.indexOf(rule.strContains) < 0) continue;
+
+    // Parsing options
+    if (rule.parser) {
+      // If we tried to parse it already and failed, let's not even parse it again
+      if (parsedAs[rule.parser] === null) {
+        continue;
+      }
+      // It's been parsed correctly already, perfect!
+      else if (parsedAs[rule.parser]) { }
+      else if (rule.parser === 'json') {
+        try {
+          parsedAs.json = JSON.parse(msg);
+        }
+        catch(ex) {
+          // Unable to parse it as it was written in the rule, skip it!
+          parsedAs.json = null;
+          continue;
+        }
+      }
+      parsedMsg = parsedAs[rule.parser];
+    }
+
+    // Parsed message matching options
+    if (typeof rule.bodyMatches === 'object' && isObject(parsedMsg)) {
+      if (!objectMatches(parsedMsg, rule.bodyMatches)) continue;
+    }
+
+    return BACKENDS[rule.target];
+  }
+  return null;
+}
+
+// The good stuff starts here
+const ServerClass = (config.server || {}).proto == 'tcp' ? TCPServer : UDPServer;
+server = new ServerClass(config.server || {});
+server.listen(async (msg) => {
+  const be = findBackend(msg);
+  if (!be) {
+    console.warn(`WARN: Could not find an appropriate backend for message: ${msg}`);
+    return;
+  }
+
+  // Send message to its corresponding backend
+  if (config.verbose) console.log(`[${be.id}] > ${msg.length} bytes`);
+  be.send(msg);
+});
+
